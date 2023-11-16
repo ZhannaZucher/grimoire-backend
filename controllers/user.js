4dca08032a287c6bb5817f00020c2338763ce7f2
ZhannaZucher <jzucher@gmail.com>
2023-11-16 17:43:02 +0100

    feat : user login

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const bcrypt = require("bcrypt")
 const User = require("../models/User")
+const jwt = require("jsonwebtoken")
 
 exports.signup = (req, res, next) => {
 	bcrypt
@@ -15,4 +17,29 @@ exports.signup = (req, res, next) => {
 		.catch(error => res.status(400).json({error}))
 	})
 	.catch(error => res.status(500).json({error}))
+}
+
+exports.login = (req, res, next) => {
+   User.findOne({ email: req.body.email })
+       .then(user => {
+           if (!user) {
+               return res.status(401).json({ error: "User not found" })
+           }
+           bcrypt.compare(req.body.password, user.password)
+               .then(valid => {
+                   if (!valid) {
+                       return res.status(401).json({ error: "Invalid password" })
+                   }
+                   res.status(200).json({
+                       userId: user._id,
+                       token: jwt.sign(
+                           { userId: user._id },
+                           process.env.SECRET_TOKEN,
+                           { expiresIn: "24h" }
+                       )
+                   })
+               })
+               .catch(error => res.status(500).json({ error }))
+       })
+       .catch(error => res.status(500).json({ error }))
 }
