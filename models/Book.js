98050e899f3af175b993a0a4267603ada9f43d8b
ZhannaZucher <jzucher@gmail.com>
2023-11-16 16:49:16 +0100

    add userId

--- models/Book.js
+++ models/Book.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const bookSchema = mongoose.Schema({
-	// userId: id MongoDB,
+	userId: {type: String, required: true},
 	title: {type: String, required: true},
 	author: {type: String, required: true},
 	imageUrl: {type: String, required: true},
@@ -9,7 +9,7 @@ const bookSchema = mongoose.Schema({
 	genre: {type: String, required: true},
 	ratings: [
 		{
-			// userId: id MongoDB,
+			userId: {type: String, required: true},
 			grade: {type: Number, required: true},
 		}
 	],
