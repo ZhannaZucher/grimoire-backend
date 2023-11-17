a199518c454386069368289bb6ce14bd6b333db6
ZhannaZucher <jzucher@gmail.com>
2023-11-17 09:07:49 +0100

    fix error msg

--- controllers/user.js
+++ controllers/user.js
@@ -23,12 +23,12 @@ exports.login = (req, res, next) => {
    User.findOne({ email: req.body.email })
        .then(user => {
            if (!user) {
-               return res.status(401).json({ error: "User not found" })
+               return res.status(401).json({ error: "Invalid credentials" })
            }
            bcrypt.compare(req.body.password, user.password)
                .then(valid => {
                    if (!valid) {
-                       return res.status(401).json({ error: "Invalid password" })
+                       return res.status(401).json({ error: "Invalid credentials" })
                    }
                    res.status(200).json({
                        userId: user._id,
