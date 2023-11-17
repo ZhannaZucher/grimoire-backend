63f45c67eeec55ef4402826e79cdc9535c02ec0a
ZhannaZucher <jzucher@gmail.com>
2023-11-17 10:15:26 +0100

    add auth middleware

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,7 @@
 const express =require("express")
 const router = express.Router()
 const booksCtrl = require("../controllers/books")
+const auth = require("../middleware/auth")
 
 router.get("/", booksCtrl.getAllBooks)
 
