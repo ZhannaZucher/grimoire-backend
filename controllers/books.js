6978af2cdc12872adaa9ac06796ee3cdcc7d8a66
ZhannaZucher <jzucher@gmail.com>
2023-11-21 18:19:05 +0100

    remove previous img from discStorage on updating

--- controllers/books.js
+++ controllers/books.js
@@ -34,10 +34,13 @@ exports.modifyBook = (req, res, next) => {
 		if (book.userId != req.auth.userId) {
 			res.status(401).json({message: "Not authorized"})
 		} else {
+			const filename = book.imageUrl.split('/images/')[1]
+			fs.unlink(`images/${filename}`, () => {
 			Book
 			.updateOne({_id: req.params.id}, {...bookObject, _id: req.params.id})
 			.then(() => res.status(200).json({message: "Object modified"})) 
 	 		.catch(error => res.status(400).json({error}))
+			})
 		}
 	})
 	.catch(error => res.status(400).json({error}))
